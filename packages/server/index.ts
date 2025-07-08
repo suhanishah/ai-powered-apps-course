2c6fca7676ef3dfa402960b833056fae10feadda
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2025-07-08 12:51:53 -0700

    Extract chat service

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,15 +1,10 @@
 import dotenv from 'dotenv';
 import type { Request, Response } from 'express';
 import express from 'express';
-import OpenAI from 'openai';
 import z from 'zod';
-import { conversationRepository } from './repositories/conversation.repository';
+import { chatService } from './services/chat.service';
 
 dotenv.config();
-
-const client = new OpenAI({
-   apiKey: process.env.OPENAI_API_KEY,
-});
 
 const app = express();
 app.use(express.json());
@@ -41,18 +36,9 @@ app.post('/api/chat', async (req: Request, res: Response) => {
 
    try {
       const { prompt, conversationId } = req.body;
-      const response = await client.responses.create({
-         model: 'gpt-4o-mini!',
-         input: prompt,
-         temperature: 0.2,
-         max_output_tokens: 100,
-         previous_response_id:
-            conversationRepository.getLastResponseId(conversationId),
-      });
+      const response = await chatService.sendMessage(prompt, conversationId);
 
-      conversationRepository.setLastResponseId(conversationId, response.id);
-
-      res.json({ message: response.output_text });
+      res.json({ message: response.message });
    } catch (error) {
       res.status(500).json({ error: 'Failed to generate a response.' });
    }
