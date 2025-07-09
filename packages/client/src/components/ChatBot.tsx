1c75aa5287a442ce0310c34c9b37c3ce0f9f2746
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2025-07-09 16:24:40 -0700

    Call the backend

--- packages/client/src/components/ChatBot.tsx
+++ packages/client/src/components/ChatBot.tsx
@@ -1,4 +1,5 @@
-import type { KeyboardEvent } from 'react';
+import axios from 'axios';
+import { useRef, type KeyboardEvent } from 'react';
 import { useForm } from 'react-hook-form';
 import { FaArrowUp } from 'react-icons/fa';
 import { Button } from './ui/button';
@@ -8,11 +9,17 @@ type FormData = {
 };
 
 const ChatBot = () => {
+   const conversationId = useRef(crypto.randomUUID());
    const { register, handleSubmit, reset, formState } = useForm<FormData>();
 
-   const onSubmit = (data: FormData) => {
-      console.log(data);
+   const onSubmit = async ({ prompt }: FormData) => {
       reset();
+
+      const { data } = await axios.post('/api/chat', {
+         prompt,
+         conversationId: conversationId.current,
+      });
+      console.log(data);
    };
 
    const onKeyDown = (e: KeyboardEvent<HTMLFormElement>) => {
