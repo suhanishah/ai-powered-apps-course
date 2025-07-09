0b6634634186305576bfd7040cfa4d876ff15650
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2025-07-09 09:26:46 -0700

    Extract routes

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,23 +1,14 @@
 import dotenv from 'dotenv';
-import type { Request, Response } from 'express';
 import express from 'express';
-import { chatController } from './controllers/chat.controller';
+import router from './routes';
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(router);
+
 const port = process.env.PORT || 3000;
-
-app.get('/', (req: Request, res: Response) => {
-   res.send('Hello World!');
-});
-
-app.get('/api/hello', (req: Request, res: Response) => {
-   res.json({ message: 'Hello World!' });
-});
-
-app.post('/api/chat', chatController.sendMessage);
 
 app.listen(port, () => {
    console.log(`Server is running on http://localhost:${port}`);
