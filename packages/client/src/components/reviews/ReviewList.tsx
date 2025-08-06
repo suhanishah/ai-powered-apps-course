e59280ee183b60a7f58bf4e32b47726c33a5f5f3
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2025-08-06 12:00:56 -0700

    Display star ratings

--- packages/client/src/components/reviews/ReviewList.tsx
+++ packages/client/src/components/reviews/ReviewList.tsx
@@ -1,5 +1,6 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
+import StarRating from './StarRating';
 
 type Props = {
    productId: number;
@@ -37,7 +38,9 @@ const ReviewList = ({ productId }: Props) => {
          {reviewData?.reviews.map((review) => (
             <div key={review.id}>
                <div className="font-semibold">{review.author}</div>
-               <div>Rating: {review.rating}/5</div>
+               <div>
+                  <StarRating value={review.rating} />
+               </div>
                <p className="py-2">{review.content}</p>
             </div>
          ))}
