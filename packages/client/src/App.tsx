10826475f42af05485bc4cd5ebe43359d73da397
Moshfegh Hamedani <programmingwithmosh@gmail.com>
2025-08-18 15:44:12 -0700

    Solution to prompt engineering exercises

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,9 @@
-import ReviewList from './components/reviews/ReviewList';
+import ChatBot from './components/chat/ChatBot';
 
 function App() {
    return (
       <div className="p-4 h-screen w-full">
-         <ReviewList productId={4} />
+         <ChatBot />
       </div>
    );
 }
